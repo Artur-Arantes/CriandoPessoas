df105ff25144999568ca84c91b5a9a1ff7021434
Artur Arantes Tiraboschi <arturarantestiraboschi@Arturs-MacBook-Air.local>
2023-01-12 20:21:20 -0300

    feat:first commit

---
+++ src/main/java/br/com/criador/CriadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.criador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/criador/CriadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.criador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
